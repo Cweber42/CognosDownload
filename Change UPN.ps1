4bdf9a8f1ccb34c204aa3a736a80cc9191230fae
Cweber42 <xeniogame@gmail.com>
2017-04-27 09:41:06 -0500

    Add files via upload

---
+++ Change UPN.ps1
@@ -0,0 +1,16 @@
+﻿#Replace with the old suffix
+$oldSuffix = 'decatur.local'
+
+#Replace with the new suffix
+$newSuffix = 'decatursd.com'
+
+#Replace with the OU you want to change suffixes for
+$ou = "Ou=Students,DC=decatur,DC=local"
+
+#Replace with the name of your AD server
+$server = "DCDNSDHCP-HS.decatur.local"
+
+Get-ADUser -SearchBase $ou -filter * | ForEach-Object {
+$newUpn = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
+$_ | Set-ADUser -server $server -UserPrincipalName $newUpn
+}
